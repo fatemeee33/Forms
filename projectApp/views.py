2a5902ce1ccd88a49ae9c18ce2e07abbb44a7761
fatemeee33 <fatemebagheri98@gmail.com>
2025-01-26 13:49:09 +0330

    Initial commit

---
+++ projectApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import InputForm
+
+# Create your views here.
+def home_view(request):
+    context = {}
+    context['form'] = InputForm()
+    return render(request, "home.html", context)
